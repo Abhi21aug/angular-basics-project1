281b564c36dadac84eae73d54af2f497e6067315
Abhishek Ray <abhishek@nubewell.com>
2023-11-25 14:10:33 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  allowDidable= true
+
+allowDidableMethod(){
+  return !this.allowDidable
+}
+}
